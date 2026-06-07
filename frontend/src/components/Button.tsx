a01f171c239892690f784bd97012fb9d4453af8d
Roberto Kennedy <roberto.kpp@gmail.com>
2026-06-07 15:03:03 -0500

    feat: refatorar componente Button e atualizar página de cadastro com novos elementos

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,17 @@
+import { twMerge } from "tailwind-merge";
+
 type Props = React.ComponentProps<"button">;
 
-export function Button({ children, ...Rest }: Props) {
- return <button className="w-full bg-amber-950 py-2 text-white rounded-lg cursor-pointer"{...Rest}>{children}</button>;
+export function Button({ className, children, ...Rest }: Props) {
+  return (
+    <button
+      className={twMerge(
+        "w-full bg-gray-200 px-4 py-2 text-white rounded-[5px] cursor-pointer leading-5 font-bold",
+        className,
+      )}
+      {...Rest}
+    >
+      {children}
+    </button>
+  );
 }
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -3,13 +3,13 @@ import { Input } from "../components/Inputs";
 
 export function Home() {
   return (
-    <div className="flex justify-center h-screen bg-blue-700">
+    <div className="flex flex-col gap-4 justify-center h-screen bg-blue-700">
       <div className="flex flex-col p-7 w-100 h-195.25 bg-white rounded-2xl">
         <div className="">
           <h1 className="text-[20px] text-gray-100 font-bold">
             Crie sua conta
           </h1>
-          <span className="text-[12px] text-gray-300">
+          <span className="text-[12px] text-gray-300 leading-4">
             Informe seu nome, e-mail e senha
           </span>
         </div>
@@ -21,8 +21,17 @@ export function Home() {
           <span className="text-gray-400 text-[12px] italic">
             Mínimo de 6 digito
           </span>
-          <Button type="submit">teste</Button>
+          <Button type="submit">Cadastrar</Button>
         </form>
+      </div>
+
+      <div className="flex flex-col p-7 w-100 bg-white rounded-2xl">
+        <h2>Já uma conta?</h2>
+        <span className="text-[12px] text-gray-300 leading-4">
+          Entre agora mesmo
+        </span>
+
+        <Button className="text-gray-200 bg-gray-500 mt-6">Acessar conta</Button>
       </div>
     </div>
   );
