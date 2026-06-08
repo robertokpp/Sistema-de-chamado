888e7d5976c1d39fc225a82ad437b32dfe7884db
Roberto Kennedy <roberto.kpp@gmail.com>
2026-06-08 17:28:57 -0500

    feat: adicionar tailwind-merge e atualizar página de cadastro

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -3,36 +3,47 @@ import { Input } from "../components/Inputs";
 
 export function Home() {
   return (
-    <div className="flex flex-col gap-4 justify-center h-screen bg-blue-700">
-      <div className="flex flex-col p-7 w-100 h-195.25 bg-white rounded-2xl">
-        <div className="">
-          <h1 className="text-[20px] text-gray-100 font-bold">
-            Crie sua conta
-          </h1>
-          <span className="text-[12px] text-gray-300 leading-4">
-            Informe seu nome, e-mail e senha
-          </span>
+    <div className="bg-blue-700">
+
+      <div className="flex flex-col p-4 gap-4 items-center border bg-white ">
+
+
+        <div className="flex flex-col p-7 w-100 border rounded-2xl">
+          <div>
+            <h1 className="text-[20px] text-gray-100 font-bold">
+              Crie sua conta
+            </h1>
+            <span className="text-[12px] text-gray-300 leading-4">
+              Informe seu nome, e-mail e senha
+            </span>
+          </div>
+
+          <form className="py-10 flex flex-col gap-4">
+            <Input legend="Name" placeholder="Digite o nome completo" />
+            <Input legend="E-mail" placeholder="exemplo@email.com" />
+            <Input legend="Senha" placeholder="Digite sua senha" />
+            <span className="text-gray-400 text-[12px] italic">
+              Mínimo de 6 digito
+            </span>
+            <Button type="submit">Cadastrar</Button>
+          </form>
         </div>
 
-        <form className="py-10 flex flex-col gap-4">
-          <Input legend="Name" placeholder="Digite o nome completo" />
-          <Input legend="E-mail" placeholder="exemplo@email.com" />
-          <Input legend="Senha" placeholder="Digite sua senha" />
-          <span className="text-gray-400 text-[12px] italic">
-            Mínimo de 6 digito
+        <div className="flex flex-col p-7 w-100 border  rounded-2xl">
+          <h2>Já uma conta?</h2>
+          <span className="text-[12px] text-gray-300 leading-4">
+            Entre agora mesmo
           </span>
-          <Button type="submit">Cadastrar</Button>
-        </form>
+
+          <Button className="text-gray-200 bg-gray-500 mt-6">
+            Acessar conta
+          </Button>
+        </div>
+
+
       </div>
 
-      <div className="flex flex-col p-7 w-100 bg-white rounded-2xl">
-        <h2>Já uma conta?</h2>
-        <span className="text-[12px] text-gray-300 leading-4">
-          Entre agora mesmo
-        </span>
 
-        <Button className="text-gray-200 bg-gray-500 mt-6">Acessar conta</Button>
-      </div>
     </div>
   );
 }
