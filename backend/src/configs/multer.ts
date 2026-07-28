0545569b64ce7aab50e50cefcae5bfb7bca9eb02
Roberto Kennedy <roberto.kpp@gmail.com>
2026-07-28 18:02:57 -0500

    feat: aprimora configuração do multer para upload de avatar, incluindo validação de tipos de arquivo e nomeação baseada no ID do usuário

--- backend/src/configs/multer.ts
+++ backend/src/configs/multer.ts
@@ -1,16 +1,26 @@
 import multer from "multer";
+import path from "node:path";
 
+const allowedMimeTypes = ["image/png", "image/jpeg", "image/webp"];
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/avatar");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    const userId = req.user?.id;
+
+    if (!userId) {
+      return cb(new Error("Não foi possível realizar o upload da imagem"), "");
+    }
+    
+    if (!allowedMimeTypes.includes(file.mimetype)) {
+      return cb(new Error("Formato de arquivo não permitido"), "");
+    }
+
+    const extName = path.extname(file.originalname);
+    cb(null, `${userId}${extName}`);
   },
 });
-
-
 
 export const upload = multer({ storage });
